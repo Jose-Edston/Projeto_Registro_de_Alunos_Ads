58314cc2c9108d166b43704cd9ba434922b280e6
Alcione Francisco de Borba <alcionefranciscodeBorba@gmail.com>
2023-06-01 20:18:49 -0300

    segundo commit

---
+++ accounts/views.py
@@ -0,0 +1,32 @@
+from multiprocessing import AuthenticationError
+from django.shortcuts import redirect, render
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+# Create your views here.
+# Home do Projeto.
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        check_user = auth.authenticate(username=username, password=password)
+        
+        if check_user == None:
+            messages.error(request, message='Usario ou senha inválidos.')
+            return redirect('login')
+        else:
+            auth.login(request, check_user)
+            return redirect('index')
+
+
+        return render(request, 'pages/login.html')
+
+    else:
+        return render(request, 'pages/login.html')
+
+def logout(request):
+    AuthenticationError.logout(request)
+    return redirect('login')
